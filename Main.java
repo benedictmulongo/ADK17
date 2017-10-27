63e8800a899d633dadff5f1a992110599e75e63d
twingard <31848330+twingard@users.noreply.github.com>
2017-10-27 15:35:43 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,36 @@
-package com.company;
-
 import java.awt.datatransfer.FlavorEvent;
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.*;
+import java.io.*;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
+	Scanner sc = new Scanner(System.in);
+	int antal_horn = sc.nextInt();
+	int s = sc.nextInt();
+	int t = sc.nextInt();
+	int antal_kanter = sc.nextInt();
 
+	FlowNetwork flw = new FlowNetwork(antal_horn);	
+	
+	for (int i = 0; i < antal_kanter; i++) {
+	    int start = sc.nextInt();
+	    int slut = sc.nextInt();
+ 	    int kap = sc.nextInt();
+	    if (start == antal_horn-1) {
+	    	flw.insert(0,slut,0,kap);
+	    } else if (slut == antal_horn) {
+		flw.insert(start,antal_horn-1,0,kap);
+	    } else {
+	    	flw.insert(start,slut,0,kap);
+	    }
+	}
 	// write your code here
-        FlowNetwork flw = new FlowNetwork(7);
-        //***
+    
+        /*
         flw.insert(0,1,0,1);
         flw.insert(0,2,0,1);
        // flw.insert(0,3,0,5);
@@ -30,19 +48,19 @@ public class Main
         flw.insert(4,6,0,1);
 
         flw.insert(5,6,0,1);
-        //***
+        */
 
         //System.out.println(" vw " + flw.edge(0,1));
         //ArrayList<EDGES> g = flw.getConnectedTo(0);
         //for(EDGES e : g)
         //    System.out.println(" -> " + e);
         //flw.findAugment(flw,0,6);
-        System.out.println(" Shortest augmented path " + flw.findShortest(flw,0,6));
-        print(flw,7);
-        augment(flw,0,6);
+        System.out.println(" Shortest augmented path " + flw.findShortest(flw,0,antal_horn-1));
+        print(flw,antal_horn);
+        augment(flw,0,antal_horn-1);
 //        System.out.println("bottleneck path " + flw.bottleneck(0,6));
         System.out.println("After augmenting !!!?? ");
-        print(flw,7);
+        print(flw,antal_horn);
 
         //System.out.println("bottleneck path " + );
 /*        for(int i = 0; i < flw.parentOf.length; i++)
@@ -76,51 +94,35 @@ public class Main
         flw.insert(0,1,3,7);
                 flw.insert(0,2,6,6);
                 flw.insert(0,3,5,5);
-
                 flw.insert(1,3,1,1);
                 flw.insert(1,4,2,2);
-
                 flw.insert(2,3,0,3);
                 flw.insert(2,5,6,9);
-
                 flw.insert(3,4,4,5);
                 flw.insert(3,6,2,3);
-
                 flw.insert(4,6,6,6);
-
                 flw.insert(5,6,6,8);*/
 /*
         flw.insert(0,1,0,7);
                 flw.insert(0,2,0,6);
                 flw.insert(0,3,0,5);
-
                 flw.insert(1,3,0,1);
                 flw.insert(1,4,0,2);
-
                 flw.insert(2,3,0,3);
                 flw.insert(2,5,0,9);
-
                 flw.insert(3,4,0,5);
                 flw.insert(3,6,0,3);
-
                 flw.insert(4,6,0,6);
-
                 flw.insert(5,6,0,8);*/
 /*
-
         flw.insert(0,1,3,7);
                 flw.insert(0,2,5,6);
                 flw.insert(0,3,2,5);
-
                 flw.insert(1,3,1,1);
                 flw.insert(1,4,2,2);
-
                 flw.insert(2,3,1,3);
                 flw.insert(2,5,4,9);
-
                 flw.insert(3,4,2,5);
                 flw.insert(3,6,2,3);
-
                 flw.insert(4,6,4,6);
-
                 flw.insert(5,6,4,8);*/
