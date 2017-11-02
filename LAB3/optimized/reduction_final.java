723574a9a0184dfb412e47165a875c98afdf2326
Benedith Mulongo <benedith@kth.se>
2017-11-02 20:31:22 +0100

    Add files via upload

---
+++ LAB3/optimized/reduction_final.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+/**
+ * Created by ben on 2017-11-02.
+ */
+public class reduction_final
+{
+    //*  read data from bipartite
+    // create file
+    // write data to targetFile
+/*    FrombipartiteToMaxflow <- data redution
+    PrintWriter output = new PrintWriter(FrombipartiteToMaxflow);
+    output.println(data);
+    ----------------------------------
+    read data from output with Scanner
+        call NetworkFlow
+                printn*/
+
+}
